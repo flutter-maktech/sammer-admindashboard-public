dcc8a232dace6224226e52c25fcc6c30e9723fde
Tuhin Ikbal Eyamin <codereyamin@gmail.com>
2026-06-08 17:45:40 +0600

    update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "909880344" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "388003640" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
